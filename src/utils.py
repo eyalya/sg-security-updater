4a8a58bc2a2362414681cc097169740042e6871b
eyal yaish <eyalyaish@gmail.com>
2022-06-13 11:25:03 +0300

    added utils functions

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,14 @@ class ComputerInfo():
             ip = get('https://api.ipify.org').text
             ip += "/32"   
             return ip
+
+    @classmethod
+    def RetrieveOldIp(cls, file):
+        with open(file) as json_file:
+            data = json.load(json_file)
+            return data
+    
+    @classmethod
+    def WriteToJasonFile(cls, file, data):
+        with open(file, 'w') as outfile:
+            json.dump(data, outfile)
